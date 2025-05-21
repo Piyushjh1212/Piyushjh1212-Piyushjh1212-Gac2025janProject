1fe66504f128b00a5e3d116e7a2b947b899ed6dd
Piyush jhariya <piyushjhariya65@gmail.com>
2025-05-22 00:55:24 +0530

    update maiin jsx

--- AdminPanel/src/main.jsx
+++ AdminPanel/src/main.jsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import GacContextProvider from "./Context/Gaccontext.jsx"; // 👈 path sahi rakho
 import ImageProvider from "./Context/ImageContext.jsx"; // 👈 path sahi rakho
+import { VideoProvider } from "./Context/Videocontext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ImageProvider>
+      <VideoProvider>
       <GacContextProvider>
         {" "}
         {/* 👈 Yeh zaroori hai */}
         <App />
       </GacContextProvider>
+      </VideoProvider>
     </ImageProvider>
   </StrictMode>
 );
