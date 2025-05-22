0ca6b17f39cd6ddcc990f6f947df3ab9db613185
Piyush jhariya <piyushjhariya65@gmail.com>
2025-05-22 11:47:05 +0530

    Home Page

--- Frontend/src/HomeComponent/HomePage/Homepage.jsx
+++ Frontend/src/HomeComponent/HomePage/Homepage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./HomePage.css"; // Make sure the filename matches
 
-export default function HomePage() {
+
+export default function HomePage({contactRef}) {
+
+   const handleExploreClick = () => {
+     contactRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+ 
   return (
     <div className="home-container" id="home">
       <div className="home-container-2">
@@ -13,7 +20,7 @@ export default function HomePage() {
           Unlock your potential with expert-led courses, interactive lessons,
           and a supportive community to help you achieve your learning goals.
         </p>
-        <button className="home-explore-btn">Explore Courses</button>
+        <button className="home-explore-btn" onClick = {handleExploreClick}>Explore Courses</button>
       </div>
     </div>
   );
--- Frontend/src/HomePage/Home.jsx
+++ Frontend/src/HomePage/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import HomePage from '../HomeComponent/HomePage/Homepage'
-import Courses from '../HomeComponent/CoursesPage/Courses'
 import Contact from '../HomeComponent/Contact/Contact'
 import BrandPartners from '../HomeComponent/BrandPatners/Brandpatners'
 import CertificatePage from '../HomeComponent/Certificates/Certificates'
+import CoursesPage from '../HomeComponent/CoursesPage/Courses'
+import { useRef } from 'react'
+
+
 
 export default function Home() {
+    const contactRef = useRef(null);
   return (
     <>
-        <HomePage/>
-        <Courses/>
+        <HomePage contactRef={contactRef}/>
+        <CoursesPage innerRef={contactRef}/>
         <BrandPartners/>
         <CertificatePage/>
-        <Contact/>
+        <Contact />
         
     </>
   )
